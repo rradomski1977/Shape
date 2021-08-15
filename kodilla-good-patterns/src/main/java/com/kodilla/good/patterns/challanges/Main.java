d0b7b5b8cf7cf29a9831ffc7e67308ce6cf28593
rradomski1977 <77922354+rradomski1977@users.noreply.github.com>
2021-08-15 12:36:24 +0200

    Update Main.java

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challanges/Main.java
@@ -1,2 +1,13 @@
-package com.kodilla.good.patterns.challanges;public class Main {
+package com.kodilla.good.patterns.challanges;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        movieStore.getMovies().entrySet().stream()
+                .map(stringListEntry -> stringListEntry.getValue())
+                .flatMap(stringList -> stringList.stream())
+                .map(title -> title + "!")
+                .forEach(System.out::print);
+    }
 }
